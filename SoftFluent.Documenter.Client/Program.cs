eecf72468f96cc66873dd46ec8a8b5482bb0c0c1
Hugo CARNICELLI <hugocarnicelli@gmail.com>
2015-12-16 10:32:08 +0100

    Reorganizing solution.

---
+++ SoftFluent.Documenter.Client/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using SoftFluent.Documenter;
+
+namespace SoftFluent.Documenter.Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // controls
+            if (args.Length < 1)
+            {
+                Console.WriteLine("Not enough arguments!");
+            }
+            if (args.Length > 1)
+            {
+                Console.WriteLine("Too much arguments!");
+            }
+
+            Documenter documenter = new Documenter();
+            documenter.RenderDirectory(args[0]);
+        }
+    }
+}
